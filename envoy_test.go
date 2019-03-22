00dfe7fe9bd99435d2081cd121e6df2032b350fe
Saravana Periyasamy <saravanakumar.periyasamy@gmail.com>
2019-03-22 10:43:38 -0500

    More test data to test

--- envoy_test.go
+++ envoy_test.go
@@ -15,56 +15,51 @@ func TestMakeEnvoyClusters(t *testing.T) {
 	envoyClustersChan := make(chan []envoycache.Resource)
 	go makeEnvoyClusters(envoyClustersChan)
 	envoyClusters := <-envoyClustersChan
-	if len(envoyClusters) != 833 {
+	if len(envoyClusters) != 959 {
 		t.Error("No Envoy Clusters created")
 	}
 }
 
 func setupEnvoyTest() {
-	ttcIngressCacheStore := &k8scache.FakeCustomStore{
-		ListKeysFunc: func() []string {
-			return []string{"test-namespace1/test-ingress1"}
-		},
-		ListFunc: func() []interface{} {
-			fakeIngresses, err := fakeIngress()
-			if err != nil {
-				log.Fatal("Failed to setup test data")
-			}
-			ingressList := make([]interface{}, 0, len(fakeIngresses))
-			for i := 0; i < len(fakeIngresses); i++ {
-				ingressList = append(ingressList, &fakeIngresses[i])
-			}
-			return ingressList
-		},
-	}
-	//tteIngressCacheStore := &k8scache.FakeCustomStore{
-	//	ListKeysFunc: func() []string {
-	//		return []string{"test-namespace1/test-ingress1"}
-	//	},
-	//}
+
 	k8sClusters = []*k8sCluster{
 		{
-			name:              "test-ttc",
-			zone:              TTC,
-			ingressCacheStore: ttcIngressCacheStore,
+			name: "cluster1",
+			zone: TTC,
 		},
-		//{
-		//	name: "test-tte",
-		//	zone: TTE,
-		//	ingressCacheStore: tteIngressCacheStore,
-		//},
+		{
+			name: "cluster2",
+			zone: TTE,
+		},
+	}
+
+	for _, k8sCluster := range k8sClusters {
+		k8sCluster.ingressCacheStore = &k8scache.FakeCustomStore{
+			ListKeysFunc: func() []string {
+				return []string{"test-namespace1/test-ingress1"}
+			},
+			ListFunc: k8sCluster.fakeIngress,
+		}
 	}
 }
 
-func fakeIngress() ([]v1beta1.Ingress, error) {
+func (c *k8sCluster) fakeIngress() []interface{} {
 	ingressList := v1beta1.IngressList{}
-	reader, err := os.Open("test-data/ingressList.yml")
+	reader, err := os.Open("test-data/" + c.name + "-ingressList.yml")
 	if err != nil {
-		return nil, err
+		log.Fatal("Failed to setup test data")
 	}
 	err = yaml.NewYAMLOrJSONDecoder(reader, 100).Decode(&ingressList)
 	if err != nil {
-		return nil, err
+		log.Fatal("Failed to setup test data")
 	}
-	return ingressList.Items, nil
+	fakeIngresses := ingressList.Items
+	if err != nil {
+		log.Fatal("Failed to setup test data")
+	}
+	ingressObjects := make([]interface{}, 0, len(fakeIngresses))
+	for i := 0; i < len(fakeIngresses); i++ {
+		ingressObjects = append(ingressObjects, &fakeIngresses[i])
+	}
+	return ingressObjects
 }
